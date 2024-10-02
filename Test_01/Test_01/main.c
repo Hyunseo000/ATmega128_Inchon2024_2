a730878a0b11884d9faa4071319c1702503c8c8e
unknown <bhs7816@gmail.com>
2024-10-02 15:40:39 +0900

    1

---
+++ Test_01/Test_01/main.c
@@ -0,0 +1,18 @@
+/*
+ * Test_01.c
+ *
+ * Created: 2024-10-02 오후 12:49:24
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
